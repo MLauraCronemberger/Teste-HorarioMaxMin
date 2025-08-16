08cb57c919ebaa4abc720edb3cfb755fce2299c1
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2025-08-16 13:23:04 -0300

    Fazendo cálculo considerando sempre intervalos de horas exatas

--- src/main/java/com/example/demo/HorarioMaxMinController.java
+++ src/main/java/com/example/demo/HorarioMaxMinController.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,19 @@ public class HorarioMaxMinController {
 	List<HorarioMaxMin> findAll(){
 		List<HorarioMaxMin> dados = repository.findAll();
 		return dados;
+	}
+	
+	@GetMapping(value="/intervaloHorarios/{id}")
+	int pegarIntervalo(@PathVariable Integer id) {
+		LocalTime horarioMinimo = repository.findById(id).get().getHorarioMin();
+		LocalTime horarioMaximo= repository.findById(id).get().getHorarioMax();
+		
+		int intervaloHorarios = Duration.between(horarioMinimo, horarioMaximo).toHoursPart();
+		
+		int intervaloFinal = intervaloHorarios * 2 + 1;
+		
+		return intervaloFinal;
+		
 	}
 
 }
