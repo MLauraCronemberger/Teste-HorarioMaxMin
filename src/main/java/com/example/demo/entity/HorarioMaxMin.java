fbb2b0233fc907f213d14115bbc1ee794d2fd99b
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2025-08-15 22:07:49 -0300

    Configurando Project para Teste

---
+++ src/main/java/com/example/demo/entity/HorarioMaxMin.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class HorarioMaxMin {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nomeServico;
+	private LocalTime horarioMin;
+	private LocalTime horarioMax;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomeServico() {
+		return nomeServico;
+	}
+	public void setNomeServico(String nomeServico) {
+		this.nomeServico = nomeServico;
+	}
+	public LocalTime getHorarioMin() {
+		return horarioMin;
+	}
+	public void setHorarioMin(LocalTime horarioMin) {
+		this.horarioMin = horarioMin;
+	}
+	public LocalTime getHorarioMax() {
+		return horarioMax;
+	}
+	public void setHorarioMax(LocalTime horarioMax) {
+		this.horarioMax = horarioMax;
+	}
+
+	
+}
